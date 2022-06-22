1727ff9905ca5000332fd4fe8af4398534a0a627
singithi-kandage <singithikandage@gmail.com>
2022-06-22 17:22:00 -0400

    Added coinMarketData page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-import "./App.css";
+import Header from "components/header";
+import CoinMarketData from "pages/coinMarketData";
 
-function App() {
-  return <div className="App"></div>;
-}
+import "./App.scss";
+
+export const history = createBrowserHistory();
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Header headerText={"Coin Gecko Dashboard"} />
+
+      <main className="main">
+        <Router>
+          <Routes>
+            <Route path="/" element={<CoinMarketData />} />
+          </Routes>
+        </Router>
+      </main>
+    </div>
+  );
+};
 
 export default App;
